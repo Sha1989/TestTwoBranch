678634b5a850b6fca491663bbd5463e59c6ca6c4
sha1989 <shashank1989bansal@gmail.coml>
2020-05-19 21:25:50 +0530

    Commit - 19_May

--- src/test/java/com/test/file/TestClass.java
+++ src/test/java/com/test/file/TestClass.java
@@ -3,23 +3,21 @@ package com.test.file;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.main.utilty.BaseClass;
+import com.main.utilty.FileReaderManager;
 import com.main.utilty.Logz;
-import com.main.utilty.ObjectClass;
 
 public class TestClass extends BaseClass {
-	
+
 	@com.main.utilty.JiraPolicy(logTicketReady = true)
 	@Test
 	public static void verifyUrl() throws Exception {
 
-		ObjectClass obj = new ObjectClass();
-		
 		logger = extent.createTest("--------- TestClass -----------");
 		Thread.sleep(2000);
 
-		obj.getTestpage().enterDataSearchField();
+		FileReaderManager.getInstance().getTestOneInstance().enterDataSearchField();
 		Logz.message("----- Done -----");
-		Assert.assertEquals("test", "nones");
+		Assert.assertEquals("test", "test");
 	}
 
 }
